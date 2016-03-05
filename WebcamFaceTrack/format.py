5f2ad6437c4af968e4d2061b7f7175f19d3a5a97
AWilcke <arthur.wilcke@gmail.com>
2016-03-05 18:45:13 +0000

    Added function that saves faces every 5 seconds

--- WebcamFaceTrack/format.py
+++ WebcamFaceTrack/format.py
@@ -73,5 +73,38 @@ def videoDic(name):
             cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255),2)
           
         cv2.imshow('Recording...',frame)
+    #cleaning up
     os.chdir('..')
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+def takePic(path):
+    lastTime = time.time()
+    video_capture = cv2.VideoCapture(0)
+    cv2.startWindowThread()
+    cv2.namedWindow('Photobooth')
+    faceCascade = cv2.CascadeClassifier(cascadePath)
+    i = 0 #img number
+    while(True):
+        ret, frame = video_capture.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=10,
+                minSize=(50,50),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+
+        for (x, y, w, h) in faces:
+            face = frame[y:y+h, x:x+w]
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255),2)
+
+        cv2.imshow('Photobooth', frame)
+
+        if time.time() - lastTime>5:
+            cv2.imwrite(path + '/' + str(i) + '.jpg', face)
+            lastTime = time.time()
+            i+=1
+    video_capture.release()
     cv2.destroyAllWindows()
