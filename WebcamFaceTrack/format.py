ba5814d1c883b07d260e54b897d0ba6cf05082e3
AWilcke <arthur.wilcke@gmail.com>
2016-03-05 16:25:34 +0000

    Everything is pretty now

--- WebcamFaceTrack/format.py
+++ WebcamFaceTrack/format.py
@@ -2,6 +2,7 @@ import os
 from PIL import Image
 import time
 import cv2
+import re
 
 cascadePath = "haarcascade_frontalface_default.xml"
 
@@ -36,12 +37,18 @@ def createPerson(name):
 def videoDic(name):
     #initialise stuff
     video_capture = cv2.VideoCapture(0)
-    cv2.startWindowThread()
-    cv2.namedWindow("Recording...")
+    cv2.startWindowThread() #start display
+    cv2.namedWindow("Recording...") #name display
     faceCascade = cv2.CascadeClassifier(cascadePath)
-    i = 0
     number = getPersonNumber('dic.txt', name)
     os.chdir('TrainingData')
+    i = 0   #index for pic number
+    pics = [pic for pic in os.listdir(os.getcwd())]
+    for pic in pics:
+        match = re.search(str(number) + '_(\d\d*).*', pic)
+        if match and int(match.group(1))>i:
+            i = int(match.group(1))
+    i+=1 #will be one more than max index for 
     start = time.time()
     lastRecord = start
     #capture for 10s
@@ -57,7 +64,7 @@ def videoDic(name):
         )
         
         #get the pics
-        if time.time() - lastRecord>0.25:
+        if time.time() - lastRecord>0.33:
             cv2.imwrite(str(number) + '_' + str(i) + '.jpg', frame)
             lastRecord = time.time()
             i+=1
